c7ec42aa0290d1820cb4d24b354d2869fc322a2e
Rasimul <rasimul29928@gmail.com>
2024-06-12 03:50:26 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 import express from "express";
 // import dotenv from "dotenv";
 // dotenv.config();
-import 'dotenv/config';
+import "dotenv/config";
 import "./db";
 const app = express();
 
 import authRouter from "#/routes/auth";
 import photoRouter from "#/routes/image";
-import userRouter from '#/routes/user';
+import userRouter from "#/routes/user";
 import { PORT } from "./utils/variables";
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('src/public'))
+app.use(express.static("src/public"));
 app.use("/auth", authRouter);
 app.use("/photo", photoRouter);
 app.use("/user", userRouter);
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
 });
+
+//ssh -i 'Ai Skinify.pem' ubundu@ec2-16-171-112-238.eu-north-1.compute.amazonaws.com   //to cunnect your cloud ubuntu machine
